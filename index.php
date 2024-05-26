ab7635bff815c9cc83b6fd4265fb9609afca8235
David I <55946833+desarius82@users.noreply.github.com>
2024-05-26 12:15:45 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<title>tbilisi bus</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+<meta name="description" content="tbilisi bus">
+<meta name="Keywords" content="tbilisi bus, tbilisi, bus">
+</head>
+<body>
+<nav>
+<a href="index.php"><img src="images/bus.png" alt="logo" align="left" border="0"></a>
+<ul>
+<li><a href="index.php">მთავარი</a></li>
+<li><a href="about.php">შესახებ</a></li>
+<li><a href="contact.php">კონტაქტი</a></li>
+</ul>
+</nav>
+<div class="pic">
+
+<!--php code--> 
+<br>
+<form action="bus.php" method="get">
+<textarea name = "textarea" cols = "40" rows = "10" maxlength = "4" <input type="number" style="font-size: 14px" onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="შეიყვანეთ გაჩერების ID"></textarea>
+<input type = "submit" value = "OK">
+</form>
+</div>
+<center>
+<b>tbilisi <font color="#FFD800">bus</font></b>
+<h6>2017</h6>
+</center>
+</body>
+</html>
