535360ee2e888545e4526194e1d81da1d948432b
David I <55946833+desarius82@users.noreply.github.com>
2024-05-26 12:18:16 +0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<title>tbilisi bus</title>
+</head>
+<body>
+<nav>
+<a href="index.php"><img src="images/bus.png" alt="logo" align="left" border="0"></a>
+<ul>
+<li><a href="index.php">მთავარი</a></li>
+<li><a href="about.php">შესახებ</a></li>
+<li><a href="contact.php">კონტაქტი</a></li>
+</ul>
+</nav>
+
+<div class="pic">
+<!--php kodi--> 
+<br>
+საიტის საშუალებით შესაძლებელია ავტობუსების მოსვლის განრიგის გაგება
+</div>
+<center>
+<b>tbilisi <font color="#FFD800">bus</font></b>
+<h6>2017</h6>
+</center>
+</body>
+</html>
